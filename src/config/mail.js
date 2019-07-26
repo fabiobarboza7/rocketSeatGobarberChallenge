0656a70c125de7961ea5363f6315099450dcaa93
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-26 00:07:03 -0300

    setup nodemailer and mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'bf68c0c53c2c39',
+    pass: '05233a5d03725b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
